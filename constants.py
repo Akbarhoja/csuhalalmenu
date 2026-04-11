d1ef4c502812ab9d052bf2a146e3a88a498a6ccf
Akbaree <84280859+Akbarhoja@users.noreply.github.com>
2026-04-10 20:00:44 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 BOT_BUTTON_TEXT = "Todays Halal Menu"
-INTRO_MESSAGE = "Meow, meow! We have these halal options for you :)"
+INTRO_MESSAGE = "Meow, meow, Nigga! We have these halal options for you :)"
 NO_ITEMS_MESSAGE = "Meow, meow! No halal options were found for today."
 NUTRISLICE_BASE_URL = "https://csumenus.nutrislice.com"
 MEAL_ORDER = ("Breakfast", "Lunch", "Dinner")
